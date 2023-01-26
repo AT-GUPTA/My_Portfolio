c67575e20dfc6b1b6e5a2286e9140ef8a9944c3e
Atul Gupta <atulg@countable.co>
2023-01-26 04:14:49 -0500

    Added Achievements, Contact, Work exp

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
-var arr = [];
-for (let i = 1; i <= 11; i++) arr[i - 1] = document.getElementById("loading" + i);
-
-window.addEventListener("scroll", scrollEvent);
-function scrollEvent() {
-	for (let i = 0; i < 11; i++) if (isInView(arr[i])) arr[i].classList.add("loading" + (i + 1));
-}
-
 function isInView(elem) {
 	var distance = elem.getBoundingClientRect();
 	return (
@@ -15,3 +7,28 @@ function isInView(elem) {
 		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
 	);
 };
+var loader = [];
+for (let i = 1; i <= 11; i++) loader[i - 1] = document.getElementById("loading" + i);
+
+window.addEventListener("scroll", scrollEvent);
+function scrollEvent() {
+	for (let i = 0; i < 11; i++) if (isInView(loader[i])) loader[i].classList.add("loading" + (i + 1));
+}
+
+var form = document.getElementById("contact-form");
+form.addEventListener("submit", SendMail);
+
+function SendMail(e) {
+	e.preventDefault();
+	var args = {
+		from_name: document.getElementById("name").value,
+		from_email: document.getElementById("email").value,
+        to_name: "Atul",
+		subject: document.getElementById("subject").value,
+		message: document.getElementById("message").value,
+	};
+	emailjs.send("service_4p75rwl", "template_ido2q0a", args).then(() => {
+		alert("Message sent!");
+	});
+	form.reset();
+}
