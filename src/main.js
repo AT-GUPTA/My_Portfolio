f067d43d1ce48894d94745b18f3956a3214d629d
Atul Gupta <atulg@12221@gmail.com>
2024-08-03 00:44:08 -0400

    Responsiveness sections updated for About, Exp, Skills

--- src/main.js
+++ src/main.js
@@ -1,34 +1,66 @@
-function isInView(elem) {
-	var distance = elem.getBoundingClientRect();
-	return (
-		distance.top >= 0 &&
-		distance.left >= 0 &&
-		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
-	);
-};
-var loader = [];
-for (let i = 1; i <= 11; i++) loader[i - 1] = document.getElementById("loading" + i);
+document.addEventListener("DOMContentLoaded", function () {
+  var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: "#spying",
+  });
+  var progressBars = document.querySelectorAll(".progress-bar");
 
-window.addEventListener("scroll", scrollEvent);
-function scrollEvent() {
-	for (let i = 0; i < 11; i++) if (isInView(loader[i])) loader[i].classList.add("loading" + (i + 1));
+  function animateProgressBars() {
+    if (isInView(document.getElementById("skills"))) {
+      progressBars.forEach(function (progressBar) {
+        progressBar.style.width = progressBar.getAttribute("data-width");
+      });
+      window.removeEventListener("scroll", animateProgressBars);
+    }
+  }
+
+  window.addEventListener("scroll", animateProgressBars);
+  animateProgressBars();
+
+  var timelineItems = document.querySelectorAll(".timeline-item");
+
+  var observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("in-view");
+        } else {
+          entry.target.classList.remove("in-view");
+        }
+      });
+    },
+    { threshold: 0.5 },
+  );
+
+  timelineItems.forEach((item) => {
+    observer.observe(item);
+  });
+});
+
+function isInView(elem) {
+  var distance = elem.getBoundingClientRect();
+  return (
+    (distance.top >= 0 && distance.top < window.innerHeight / 1.75) ||
+    (distance.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      distance.bottom >= window.innerHeight / 1.75)
+  );
 }
 
+// contact me email
 var form = document.getElementById("contact-form");
 form.addEventListener("submit", SendMail);
 
 function SendMail(e) {
-	e.preventDefault();
-	var args = {
-		from_name: document.getElementById("name").value,
-		from_email: document.getElementById("email").value,
-        to_name: "Atul",
-		subject: document.getElementById("subject").value,
-		message: document.getElementById("message").value,
-	};
-	emailjs.send("service_4p75rwl", "template_ido2q0a", args).then(() => {
-		alert("Message sent!");
-	});
-	form.reset();
+  e.preventDefault();
+  var args = {
+    from_name: document.getElementById("name").value,
+    from_email: document.getElementById("email").value,
+    to_name: "Atul",
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs.send("service_4p75rwl", "template_ido2q0a", args).then(() => {
+    alert("Message sent!");
+  });
+  form.reset();
 }
