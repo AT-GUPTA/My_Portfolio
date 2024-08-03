1f1627cca761cfc7beb15d01e09223ff69c3fe0c
Atul Gupta <atulg@12221@gmail.com>
2024-08-03 03:40:26 -0400

    Finalized css

--- src/main.js
+++ src/main.js
@@ -14,25 +14,70 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   window.addEventListener("scroll", animateProgressBars);
-  animateProgressBars();
+  animateProgressBars(); // Initial check in case the section is already in view
 
+  // IntersectionObserver for timeline and award items
   var timelineItems = document.querySelectorAll(".timeline-item");
+  var awardItems = document.querySelectorAll(".award-item");
+  var aboutSection = document.querySelector("#about");
+  var contactSection = document.querySelector("#contact");
+  var projectCards = document.querySelectorAll(".project-card");
+  var headings = document.querySelectorAll(".custom-head");
 
-  var observer = new IntersectionObserver(
-    (entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("in-view");
-        } else {
-          entry.target.classList.remove("in-view");
-        }
-      });
-    },
-    { threshold: 0.5 },
-  );
+  var observerOptions = { threshold: 0.5 };
+
+  var observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate__fadeIn");
+        entry.target.classList.remove("animate__fadeOut");
+      } else {
+        entry.target.classList.add("animate__fadeOut");
+        entry.target.classList.remove("animate__fadeIn");
+      }
+    });
+  }, observerOptions);
 
   timelineItems.forEach((item) => {
     observer.observe(item);
+  });
+
+  awardItems.forEach((item) => {
+    observer.observe(item);
+  });
+
+  observer.observe(aboutSection);
+  observer.observe(contactSection);
+
+  // IntersectionObserver for project cards
+  var projectObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+      } else {
+        entry.target.classList.remove("in-view");
+      }
+    });
+  }, observerOptions);
+
+  // IntersectionObserver for headings
+  var headingObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+      } else {
+        entry.target.classList.remove("in-view");
+      }
+    });
+  }, observerOptions);
+
+  headings.forEach((heading) => {
+    heading.classList.add("fade-in");
+    headingObserver.observe(heading);
+  });
+
+  projectCards.forEach((card) => {
+    projectObserver.observe(card);
   });
 });
 
